c609128bc70549e668c04c55a1540fc7da09cc8d
carolquiterio <carolquiterio@yahoo.com.br>
2023-08-17 08:29:13 -0300

    Add project

---
+++ SchoolApp/SchoolApp/SchoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolAppApp.swift
+//  SchoolApp
+//
+//  Created by Carol Quiterio on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SchoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
